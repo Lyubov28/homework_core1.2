6aad20c4a4f3d3b5494f32679b1b24bcf27a8db4
Lyubov28 <akimova.1993@inbox.ru>
2022-03-27 23:38:08 +0300

    Add files

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener error = System.out::println;
+
+        Worker worker = new Worker(listener, error);
+        worker.start();
+
+    }
+}
